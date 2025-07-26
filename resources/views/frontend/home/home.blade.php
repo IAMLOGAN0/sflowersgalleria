c9f10e006cefe54e9fa3bb134b92bc76dcf64ddf
Soumendu <soumendugiri77492652@gmail.com>
2025-07-26 16:49:57 +0530

    git push

--- resources/views/frontend/home/home.blade.php
+++ resources/views/frontend/home/home.blade.php
@@ -44,7 +44,7 @@
     <!--============================
         SINGLE BANNER START
     ==============================-->
-    @include('frontend.home.sections.single-banner')
+    {{-- @include('frontend.home.sections.single-banner') --}}
     <!--============================
         SINGLE BANNER END
     ==============================-->
@@ -53,7 +53,7 @@
     <!--============================
         HOT DEALS START
     ==============================-->
-    @include('frontend.home.sections.hot-deals')
+    {{-- @include('frontend.home.sections.hot-deals') --}}
     <!--============================
         HOT DEALS END
     ==============================-->
@@ -91,7 +91,7 @@
     <!--============================
         WEEKLY BEST ITEM START
     ==============================-->
-    @include('frontend.home.sections.weekly-best-item')
+    {{-- @include('frontend.home.sections.weekly-best-item') --}}
     <!--============================
         WEEKLY BEST ITEM END
     ==============================-->
