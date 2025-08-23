07cfe9ecee1e5124cf092688243ae0473f0644fb
Soumendu <soumendugiri77492652@gmail.com>
2025-08-24 00:49:17 +0530

    jhghj

--- resources/views/frontend/pages/product-detail.blade.php
+++ resources/views/frontend/pages/product-detail.blade.php
@@ -179,11 +179,6 @@
                                         <button type="submit" class="btn btn-primary px-4 shadow-sm d-flex align-items-center">
                                             <i class="fas fa-shopping-cart me-2"></i> Add to Cart
                                         </button>
-                                    
-                                        <a href="{{ route('cart-details') }}" class="btn btn-primary px-4 shadow-sm d-flex align-items-center">
-                                            <i class="fas fa-shopping-cart me-2"></i> Go to Cart
-                                        </a>
-                                    
                                         <button type="button" 
                                             class="btn btn-outline-secondary rounded-circle shadow-sm d-flex align-items-center justify-content-center"
                                             data-bs-toggle="modal" data-bs-target="#exampleModal"
