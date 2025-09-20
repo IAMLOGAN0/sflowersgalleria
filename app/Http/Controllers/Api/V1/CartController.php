1a2ded913711f08b70ffb02df01697346e38f802
Soumendu <soumendugiri77492652@gmail.com>
2025-09-20 23:59:37 +0530

    api added

--- app/Http/Controllers/Api/V1/CartController.php
+++ app/Http/Controllers/Api/V1/CartController.php
@@ -71,7 +71,7 @@ class CartController extends Controller
     /** Get all cart items */
     public function getCart(Request $request)
     {
-        $items = CartItem::with('product', 'product.variants', 'product.variantsItems')
+        $items = CartItem::with('product', 'product.variants', 'product.category', 'product.productImageGalleries')
         ->where('user_id', $request->user()->id)
         ->get();
         return response()->json(['status' => 'success', 'data' => $items]);
