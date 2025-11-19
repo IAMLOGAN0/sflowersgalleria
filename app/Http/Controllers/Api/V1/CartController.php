1a6910761d726668b0f05268f5d748c2010282e7
Soumendu <soumendugiri77492652@gmail.com>
2025-11-19 05:32:05 +0530

    fix

--- app/Http/Controllers/Api/V1/CartController.php
+++ app/Http/Controllers/Api/V1/CartController.php
@@ -71,6 +71,7 @@ class CartController extends Controller
     /** Get all cart items */
     public function getCart(Request $request)
     {
+        dd(CartItem::all());
         $items = CartItem::with('product', 'product.variants', 'product.category', 'product.productImageGalleries')
         ->where('user_id', $request->user()->id)
         ->get();
