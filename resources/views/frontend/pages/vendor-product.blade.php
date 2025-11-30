6a5878e327731a84dc63014d3af2f6f33efd4f28
Soumendu <soumendugiri77492652@gmail.com>
2025-11-30 18:07:34 +0530

    wishlist remove

--- resources/views/frontend/pages/vendor-product.blade.php
+++ resources/views/frontend/pages/vendor-product.blade.php
@@ -102,7 +102,7 @@
                                             <ul class="wsus__single_pro_icon">
                                                 <li><a href="#" data-bs-toggle="modal" data-bs-target="#product-{{$product->id}}"><i
                                                             class="far fa-eye"></i></a></li>
-                                                <li><a href="#" class="add_to_wishlist" data-id="{{$product->id}}"><i class="far fa-heart"></i></a></li>
+                                                {{-- <li><a href="#" class="add_to_wishlist" data-id="{{$product->id}}"><i class="far fa-heart"></i></a></li> --}}
                                                 {{-- <li><a href="#"><i class="far fa-random"></i></a> --}}
                                             </ul>
                                             <div class="wsus__product_details">
@@ -361,8 +361,8 @@
 
                                         <ul class="wsus__button_area">
                                             <li><button type="submit" class="add_cart" href="#">add to cart</button></li>
-                                            
-                                            <li><a href="#" class="add_to_wishlist" data-id="{{$product->id}}"><i class="fal fa-heart"></i></a></li>
+
+                                            {{-- <li><a href="#" class="add_to_wishlist" data-id="{{$product->id}}"><i class="fal fa-heart"></i></a></li> --}}
                                             {{-- <li><a href="#"><i class="far fa-random"></i></a></li> --}}
                                         </ul>
                                     </form>
